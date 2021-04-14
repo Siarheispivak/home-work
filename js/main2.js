55eef91847a610978c8dd4813d7fee7ae7f1036f
Ssspivka <78437513+Ssspivka@users.noreply.github.com>
2021-04-15 00:24:47 +0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,11 +1,11 @@
-let styles = ["джаз", "бдюз"];
-styles.push("рок-н-ролл");           //Джаз, Блюз, Рок-н-Ролл
-styles.splice(-2, 1, "классика");    //Джаз, Классика, Рок-н-Ролл
-alert(styles);
-let removed = styles.shift();        //Классика, Рок-н-Ролл
-console.log(removed);
-styles.unshift("рэп", "регги");      //Рэп, Регги, Классика, Рок-н-Ролл
-alert(styles);
+// let styles = ["джаз", "бдюз"];
+// styles.push("рок-н-ролл");           //Джаз, Блюз, Рок-н-Ролл
+// styles.splice(-2, 1, "классика");    //Джаз, Классика, Рок-н-Ролл
+// alert(styles);
+// let removed = styles.shift();        //Классика, Рок-н-Ролл
+// console.log(removed);
+// styles.unshift("рэп", "регги");      //Рэп, Регги, Классика, Рок-н-Ролл
+// alert(styles);
 
 
 
@@ -15,7 +15,7 @@ alert(styles);
 
 
 
-let styles2 = ["джаз", "бдюз", "Рэп", "Регги", "Классика", "Рок-н-Ролл"];
+// let styles2 = ["джаз", "бдюз", "Рэп", "Регги", "Классика", "Рок-н-Ролл"];
 
 
-alert(styles2.slice(-1));
+// alert(styles2.slice(-1));
