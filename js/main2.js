8f32782ea18818c5f67edc9050fd8738fec70899
Ssspivka <78437513+Ssspivka@users.noreply.github.com>
2021-04-14 21:49:05 +0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,11 +1,11 @@
-// let styles = ["джаз", "бдюз"];
-// styles.push("рок-н-ролл");           //Джаз, Блюз, Рок-н-Ролл
-// styles.splice(-2, 1, "классика");    //Джаз, Классика, Рок-н-Ролл
-// alert(styles);
-// let removed = styles.shift();        //Классика, Рок-н-Ролл
-// console.log(removed);
-// styles.unshift("рэп", "регги");      //Рэп, Регги, Классика, Рок-н-Ролл
-// alert(styles);
+let styles = ["джаз", "бдюз"];
+styles.push("рок-н-ролл");           //Джаз, Блюз, Рок-н-Ролл
+styles.splice(-2, 1, "классика");    //Джаз, Классика, Рок-н-Ролл
+alert(styles);
+let removed = styles.shift();        //Классика, Рок-н-Ролл
+console.log(removed);
+styles.unshift("рэп", "регги");      //Рэп, Регги, Классика, Рок-н-Ролл
+alert(styles);
 
 
 
